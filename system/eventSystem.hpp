85a83c84cc58c60e99388c766b15b6aca13fab92
tfgl <tafanelgil@gmail.com>
2021-11-18 23:13:27 +0100

    event modularisation

--- system/eventSystem.hpp
+++ system/eventSystem.hpp
@@ -1,27 +1,115 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <map>
+#include <list>
+#include <iostream>
 #include "../engine/engine.hpp"
 #include "../components/2d_core.hpp"
 
 extern Overseer overlord;
+using pfun = void (*)(SDL_Keysym);
 
 class EventSystem: public System {
     private:
+
         char moving = 0;
         double xspeed = 0.2,
                yspeed = 0.1;
+
+        std::map<SDL_Keycode, pfun> actionMap;
+        std::list<SDL_Keycode> downKey;
+
+        void move(SDL_KeyCode key)
+        {
+            auto& rigibody = overlord.getComponent<Rigibody>( *entities.begin() );
+            auto& renderable = overlord.getComponent<Renderable>( *entities.begin() );
+
+            switch(key) {
+                case SDLK_z:
+                    rigibody.velocity.x = xspeed;
+                    rigibody.velocity.y = -yspeed;
+                    renderable.src.x = 0;
+                    moving |= 0x1;
+                    break;
+
+                case SDLK_q:
+                    rigibody.velocity.x = -xspeed;
+                    rigibody.velocity.y = -yspeed;
+                    renderable.src.x = 1024;
+                    moving |= 0x2;
+                    break;
+
+                case SDLK_s:
+                    rigibody.velocity.x = -xspeed;
+                    rigibody.velocity.y = yspeed;
+                    renderable.src.x = 2048;
+                    moving |= 0x4;
+                    break;
+
+                case SDLK_d:
+                    rigibody.velocity.x = xspeed;
+                    rigibody.velocity.y = yspeed;
+                    renderable.src.x = 3072;
+                    moving |= 0x8;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        void stopMove(SDL_KeyCode key)
+        {
+            auto& rigibody = overlord.getComponent<Rigibody>( *entities.begin() );
+
+            switch(key) {
+                case SDLK_z:
+                    moving &= 0xe;
+                    break;
+                case SDLK_q:
+                    moving &= 0xd;
+                    break;
+                case SDLK_s:
+                    moving &= 0xb;
+                    break;
+                case SDLK_d:
+                    moving &= 0x7;
+                    break;
+
+                default:
+                    break;
+            }
+
+            if( !moving ) {
+                rigibody.velocity.y = 0;
+                rigibody.velocity.x = 0;
+            }
+        }
 
     public:
         bool quit = false;
 
         void init()
-        {}
+        {
+            onKeyPress(SDLK_z, move);
+        }
+
+        bool onKeyPress(SDL_Keycode key, pfun fun, bool ignoreHolding=false)
+        {
+            return (actionMap[key] = fun) != 0;
+        }
+
+        bool onKeyRelease(SDL_Keycode key, pfun fun)
+        {
+            return (actionMap[key] = fun) != 0;
+        }
 
         void update(double dt)
         {
             SDL_Event e = {0};
             SDL_WaitEvent(&e);
+
             switch(e.type) {
                 case SDL_QUIT:
                     quit = true;
@@ -29,9 +117,12 @@ class EventSystem: public System {
 
                 case SDL_KEYDOWN:
                     keyDownEvent(e);
+                    downKey.push_back(e.key.keysym.sym);
+
                     break;
 
                 case SDL_KEYUP:
+                    downKey.remove(e.key.keysym.sym);
                     keyUpEvent(e);
                     break;
 
@@ -111,9 +202,13 @@ class EventSystem: public System {
             }
 
             if( !moving ) {
-                    rigibody.velocity.y = 0;
-                    rigibody.velocity.x = 0;
+                rigibody.velocity.y = 0;
+                rigibody.velocity.x = 0;
             }
+        }
+
+        void doAction()
+        {
         }
 };
 
